d6ee575d32e7596a279fc5d53c2707b79818928d
Ryan Govostes <rgovostes@whoi.edu>
2022-08-08 13:57:58 -0400

    Fix scope to be relative to directory for GitHub

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// Figure out the path from which this code is being served. The service
+// worker's scope must be a child of this path.
+const basepath = (
+    new URL(window.location.href).pathname.split("/").slice(0,-1).join("/")
+);
+
 // Fetch and render the README text
 (async () => {
     const response = await fetch("README.md");
@@ -9,7 +15,10 @@
 // Function to start the service worker once the database is ready
 const registerServiceWorker = () => {
     console.log("Registering service worker");
-    navigator.serviceWorker.register('worker.js', { scope: '/localzip/' });
+    navigator.serviceWorker.register(
+        "worker.js",
+        { scope: `${basepath}/localzip/` }
+    );
 };
 
 // Create an IndexedDB database
@@ -51,9 +60,8 @@ document.getElementById("load").addEventListener("click", async (e) => {
 
 // Click event handler for Display button
 document.getElementById("display").addEventListener("click", (e) => {
-    document.getElementById("image").src = "/localzip/maeby.jpg";
+    document.getElementById("image").src = `${basepath}/localzip/maeby.jpg`;
 });
-
 
 // Load event handler for image, to update caption with URL
 document.getElementById("image").addEventListener("load", (e) => {
